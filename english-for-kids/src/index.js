9e73ff3caa25681bf7873330e0983c762549881a
Ivan Holub <ivanholub.js@gmail.com>
2020-04-16 12:36:16 +0300

    feat: transform data object with categories and cards

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -1,8 +1,8 @@
 import {cards} from './js/cards';
 import {card, Card} from './js/Card';
-import {createCategory} from './js/category'
+import {Category} from './js/Category'
 
-
+console.log(cards)
 
 window.addEventListener('load', ()=>{
 
@@ -15,26 +15,26 @@ window.addEventListener('load', ()=>{
 const renderCategoriesToDom = () => {
 
     let layout = document.querySelector('.main');
-    layout.innerHTML = '';
+    // layout.innerHTML = '';
 
-    for(let i = 0; i < cards[0].length; i++){
-        let node = document.createElement('a');
-        node.setAttribute('href', '#');
-        node.classList.add('card-main');
-        let img = document.createElement('img');
-        img.classList.add('card-main__img');
-        img.setAttribute('src', `./assets/${cards[i+1][0].image}`);
-        let title = document.createElement('p');
-        title.classList.add('card-main__title');
-        title.textContent = cards[0][i];
-        node.append(img);
-        node.append(title);
-        layout.append(node);
-    } 
+    // for(let i = 0; i < cards[0].length; i++){
+    //     let node = document.createElement('a');
+    //     node.setAttribute('href', '#');
+    //     node.classList.add('card-main');
+    //     let img = document.createElement('img');
+    //     img.classList.add('card-main__img');
+    //     img.setAttribute('src', `./assets/${cards[i+1][0].image}`);
+    //     let title = document.createElement('p');
+    //     title.classList.add('card-main__title');
+    //     title.textContent = cards[0][i];
+    //     node.append(img);
+    //     node.append(title);
+    //     layout.append(node);
+    // } 
 
-    // for(let i = 0; i < 8; i++){
-    //     layout.append(new Card(cards[1][i]).createCategory())
-    // }
+    for(let i = 0; i < 8; i++){
+        layout.append(new Card(cards[1][i]).createCategory())
+    }
         
 }
 
