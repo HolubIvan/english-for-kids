8ba55b20303e5bc5b6caf4e7d618cb09fb839537
Ivan Holub <ivanholub.js@gmail.com>
2020-04-16 14:35:48 +0300

    feat: add listener to main container for chaninge categories to cards

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -1,41 +1,92 @@
 import {cards} from './js/cards';
-import {card, Card} from './js/Card';
-import {Category} from './js/Category'
+import {Card} from './js/Card';
 
-console.log(cards)
+//variables
+let layout = document.querySelector('.main');
+let categoryCard = document.querySelector('.card-main');
+let switcher = document.querySelector('.switch');
 
+
+//Window listener
 window.addEventListener('load', ()=>{
 
     if(cards){
         renderCategoriesToDom();
     }
+
+    // localStorage.setItem('isPlay', false)
 })
+
+
+//switch button listener
+// switcher.addEventListener('click', ()=>{
+//     changeGameStatus();
+    
+// })
+
 
 
 const renderCategoriesToDom = () => {
 
-    let layout = document.querySelector('.main');
-    // layout.innerHTML = '';
+    layout.innerHTML = '';
 
-    // for(let i = 0; i < cards[0].length; i++){
-    //     let node = document.createElement('a');
-    //     node.setAttribute('href', '#');
-    //     node.classList.add('card-main');
-    //     let img = document.createElement('img');
-    //     img.classList.add('card-main__img');
-    //     img.setAttribute('src', `./assets/${cards[i+1][0].image}`);
-    //     let title = document.createElement('p');
-    //     title.classList.add('card-main__title');
-    //     title.textContent = cards[0][i];
-    //     node.append(img);
-    //     node.append(title);
-    //     layout.append(node);
-    // } 
-
-    for(let i = 0; i < 8; i++){
-        layout.append(new Card(cards[1][i]).createCategory())
-    }
-        
+    createCategories(cards);
 }
 
 
+//Create cards function
+const createCards = (key) => {
+    cards[key].forEach((el)=>{
+        layout.append(new Card(el).createCategory())
+    }) 
+}
+
+
+//Create categories function
+const createCategories = (object) => {
+    for(let key in object){
+        let node = document.createElement('a');
+        node.setAttribute('href', `#${key}`);
+        node.classList.add('card-main');
+        let img = document.createElement('img');
+        img.classList.add('card-main__img');
+        img.setAttribute('src', `./assets/${object[key][0].image}`);
+        let title = document.createElement('p');
+        title.classList.add('card-main__title');
+        title.textContent = key;
+        node.append(img);
+        node.append(title);
+        layout.append(node);
+    }
+}
+
+
+//Game status changer from train to play
+// const changeGameStatus = ()=>{
+//     if(localStorage.getItem('isPlay' === false)){
+//         localStorage.setItem('isPlay', true)
+//     } else {
+//         localStorage.setItem('isPlay', false)
+//     }
+    
+// }
+
+
+layout.addEventListener('click', (event)=>{
+
+
+    // if(event.target.closest('.card-main').getAttribute('href') === '#Action (set C)'){
+    //      console.log(cards['Action (set C)'])
+    // }
+
+    for(let key in cards){
+        if(event.target.closest('.card-main').getAttribute('href') === `#${key}`){
+            layout.innerHTML = ''
+            createCards(key);
+       }
+    }
+
+    
+
+   
+})
